1f63a26519bc0e8f0e8dc75619950039e6837b39
Vinay AVS <vinayavs@gmail.com>
2021-03-29 10:36:44 +0530

    Hello World

--- Spark-Core/01-HelloSpark/HelloSpark.py
+++ Spark-Core/01-HelloSpark/HelloSpark.py
@@ -1,12 +1,41 @@
+import sys
+
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import *
 
+from lib.logger import Log4J
+from lib.utils import get_spark_app_config, load_survey_df, count_by_country
 
 if __name__ == '__main__':
     # print("Hello PySpark..!!")
-    spark = SparkSession().Builder \
-        .appName("Hello Spark") \
-        .master("Local[3]") \
+    # conf = SparkConf()
+    # conf.set("spark.app.name","Hello Spark")
+    # conf.set("spark.master","local[2]")
+
+    conf = get_spark_app_config()
+    spark = SparkSession.builder \
+        .config(conf=conf) \
         .getOrCreate()
 
-    # logger = Log4J(spark)
-    spark.stop()
+    logger = Log4J(spark)
+
+    if len(sys.argv) !=2:
+        logger.error("Usage: Hello Spark <File Name>")
+        sys.exit(-1)
+
+    logger.info("******STARTING SPARK JOB******")
+    # conf_out = spark.sparkContext.getConf()
+    # logger.info(conf_out.toDebugString())
+    survey_df = load_survey_df(spark,sys.argv[1])
+    # survey_df.show()
+    # Increasing the partitions to test wide transformations
+    partitioned_df = survey_df.repartition(2)
+
+    count_df = count_by_country(partitioned_df)
+    # count_df.show()
+    logger.info(count_df.collect())
+
+    input("Press Enter")  # For Local debugging purpose only
+
+    logger.info("******SPARK JOB FINISHED******")
+    # spark.stop()
--- Spark-Core/01-HelloSpark/lib/logger.py
+++ Spark-Core/01-HelloSpark/lib/logger.py
@@ -1,10 +1,10 @@
 class Log4J:
     def __init__(self, spark):
-        log4j = spark._jvm.org.apache.log4j
         root_class = "vin.demo.spark.examples"
         conf = spark.sparkContext.getConf()
         app_name = conf.get("spark.app.name")
-        self.logger = log4j.LogManager.getLogger(root_class +"."+ app_name)
+        log4j = spark._jvm.org.apache.log4j
+        self.logger = log4j.LogManager.getLogger(root_class + "." + app_name)
 
     def warn(self, message):
         self.logger.warn(message)
